4a4a5c2c0948eb3e77f613f830ad8dd10911fb45
Matei-Iulian Cocu <98695197+mateipiratul@users.noreply.github.com>
2025-11-28 14:30:48 +0200

    structural upd

---
+++ Grapher/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Grapher.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace grapher_1.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
