8cbe1d8d9ff24a14386125fd6710416911a6852c
Matei-Iulian Cocu <98695197+mateipiratul@users.noreply.github.com>
2025-12-07 18:30:47 +0200

    modele

---
+++ src/Grapher/Models/UserProfile.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Grapher.Models
+{
+    public class UserProfile
+    {
+        [Key, ForeignKey("User")]
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        [StringLength(100)]
+        public string? FirstName { get; set; }
+
+        [StringLength(100)]
+        public string? LastName { get; set; }
+
+        [StringLength(100)]
+        public string? JobTitle { get; set; }
+
+        public bool UsesDarkMode { get; set; } = false;
+
+        [NotMapped]
+        public string DisplayName => $"{FirstName} {LastName}";
+    }
+}
